111087a9657b88b6e61267b44a6b8a12acccfe62
David Zivanic <davidzivanic02@gmail.com>
2020-12-21 10:20:54 +0000

    Recreated project

---
+++ CuisineType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantAdvisor.Core
+{
+    public enum CuisineType
+    {
+        None,
+        English,
+        French,
+        American,
+        Italian,
+        Serbian,
+        Russian
+    }
+}
